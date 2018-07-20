467a408eb823c404d218f5fabc631c6272da0469
Jorge Demetrio Sobieski Teixeira <jorge.demetrio@alldreams.com.br>
2018-07-19 22:38:38 -0300

    Correção de didioma

--- com_demeter/src/main/php/demeter_content_plugin/demeter_content_plugin.php
+++ com_demeter/src/main/php/demeter_content_plugin/demeter_content_plugin.php
@@ -48,7 +48,7 @@ class plgContentDemeter_content_plugin extends JPlugin
      var js, fjs = d.getElementsByTagName(s)[0];
      if (d.getElementById(id)) {return;}
      js = d.createElement(s); js.id = id;
-     js.src = 'https://connect.facebook.net/en_US/sdk.js';
+     js.src = 'https://connect.facebook.net/pt_BR/sdk.js';
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));");
 		
